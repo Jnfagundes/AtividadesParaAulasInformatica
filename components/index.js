912cac1a46b9cc3e684b663feef0226906301e22
José Nilton <71674292+Jnfagundes@users.noreply.github.com>
2025-02-03 12:47:43 -0300

    Criação de mais elementos

--- components/index.js
+++ components/index.js
@@ -1,1 +1,16 @@
-document.body.style.backgroundColor = "green"
+document.body.style.backgroundColor = "rgb(48, 49, 48)";
+const body = document.body
+
+const main = document.createElement('main');//Criação da main
+body.appendChild(main); //Adiciona a main ao body
+
+//criando um link entre o css e js
+const link = document.createElement('link'); //Criando o elemento link
+link.rel = 'stylesheet'; //definindo o tipo de link
+link.href = 'index.css'; //caminho do arquivo css externo
+document.head.appendChild(link); //colocando o elemento link dentro do head
+
+
+
+
+
